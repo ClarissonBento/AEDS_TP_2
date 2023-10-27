965505a80aba1e4eb4cfa13ea3730b2f5d8b67fb
Clarisson <clarisson.bento@ufv.br>
2023-10-27 00:19:06 -0300

    Funcionando

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 #include <time.h>
 
 void trocar(int *a, int *b);
-void permutador(int cidades[], int inicio, int N, int T, int M[T][T]);
+void permutador(int cidades[], int inicio, int N, int T, int M[T][T], int *menor_custo, int *percurso);
 void matriz_aleatoria(int T, int matriz[T][T]);
-int calcula_custo(int cidades[], int T, int M[T][T]);
 void prepara_cidades(int *cidades, int *N, int partida);
+void imprime_resultados(int cidades[], int N, int T, int M[T][T], int menor_custo, int percurso[]);
 
 int main(){
     int N, T, aux; // T é pro vetor e N pro array
-    int custo = 0;
-    int menor_custo = 99999;
+    int custo = 0, menor_custo = 99999;
+
+    clock_t start_time, end_time;
+    double elapsed_time;
 
     printf("1 - Matriz aleatoria\n2 - Ler arquivo\nChoose your path or die: ");
     scanf("%i", &aux);
@@ -26,32 +28,83 @@ int main(){
 
         // Ponto de partida com as matriculas
         int partida = (4005 + 5795 + 5378) % N;
-        printf("Partida = %i\n", partida);
+        printf("\nPartida = %i", partida);
 
         // Preparando o vetor cidades
         int cidades[N];
         prepara_cidades(cidades, &N, partida);
-        
-        printf("Cidades: ");
-        for (int i = 0; i < N; i++){
-            printf("%i ", cidades[i]);
-        }
-        printf("\n");
+        printf("\nCidades: ");
+        for (int i = 0; i < N; i++) printf("%i ", cidades[i]);
+        printf("\n\n");
 
-        printf("\nMatriz aleatória:\n");
-        for (int i = 0; i < T; i++){
-            for (int j = 0; j < T; j++){
-                //printf("%i ", M[i][j]);
-                printf("Posição[%i][%i] = %i\n", i, j, M[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
+        int menor_custo = 99999;
+        int percurso[N];
 
-        permutador(cidades, 0, N, T, M);
+        start_time = clock(); // Pra considerar apenas as permutações
+
+        permutador(cidades, 0, N, T, M, &menor_custo, percurso);
+        imprime_resultados(cidades, N, T, M, menor_custo, percurso);
+
+        //elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+        elapsed_time = (double)(end_time - start_time);
+        printf("Tempo de execução: %f segundos\n\n", elapsed_time);
     }
 
     return 0;
+}
+
+void imprime_resultados(int cidades[], int N, int T, int M[T][T], int menor_custo, int percurso[]){
+
+/*  printf("\nMatriz aleatória:\n");
+    for (int i = 0; i < T; i++){
+        for (int j = 0; j < T; j++){
+            //printf("%i ", M[i][j]);
+            printf("Posição[%i][%i] = %i\n", i, j, M[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n"); 
+*/
+
+    printf("\nMenor custo = %i\n", menor_custo);
+    printf("Percurso feito = ");
+    for (int i = 0; i < N; i++)
+    {
+        printf("%i ", percurso[i]);
+    }
+    printf("\n");
+}
+
+// Função para gerar permutações recursivamente
+void permutador(int cidades[], int inicio, int N, int T, int M[T][T], int *menor_custo, int *percurso){
+    
+    if (inicio+1 == N-1) {
+        for (int i = 0; i < N; i++) {
+            printf("%i ", cidades[i]);
+        }
+
+        // Usando a matriz pra calcular o custo de cada rota/percuso
+        int custo = 0;
+        for (int i = 0; i < T; i++) {
+            int cidade_atual = cidades[i];
+            int proxima_cidade = cidades[i + 1];
+            custo += M[cidade_atual][proxima_cidade];
+        }
+
+        // Aqui captura o menor custo e o seu percurso
+        printf("| Custo = %i\n", custo);
+        if (custo < (*menor_custo)){
+            (*menor_custo) = custo;
+            for (int  i = 0; i < N; i++) percurso[i] = cidades[i];
+        }
+
+    } else {
+        for (int i = inicio+1; i < N-1; i++) {  // permutação go brr
+            trocar(&cidades[inicio+1], &cidades[i]);
+            permutador(cidades, inicio+1, N, T, M, menor_custo, percurso);
+            trocar(&cidades[inicio+1], &cidades[i]);
+        }
+    }
 }
 
 // Depois eu explico
@@ -78,41 +131,6 @@ void prepara_cidades(int *cidades, int *N, int partida){
     }
     cidades[0] = partida;
     cidades[(*N) - 1] = partida;
-}
-
-// Função para gerar permutações recursivamente
-void permutador(int cidades[], int inicio, int N, int T, int M[T][T]){
-    
-    if (inicio+1 == N-1) {
-        for (int i = 0; i < N; i++) {
-            printf("%i ", cidades[i]);
-        }
-        calcula_custo(cidades, T, M);
-
-    } else {
-        for (int i = inicio+1; i < N-1; i++) {
-            trocar(&cidades[inicio+1], &cidades[i]);
-            permutador(cidades, inicio+1, N, T, M);
-            trocar(&cidades[inicio+1], &cidades[i]);
-        }
-    }
-}
-
-int menor_custo = 9999;
-int calcula_custo(int cidades[], int T, int M[T][T]){
-    int custo = 0;
-
-    for (int i = 0; i < T; i++) {
-        int cidade_atual = cidades[i];
-        int proxima_cidade = cidades[i + 1];
-        custo += M[cidade_atual][proxima_cidade];
-    }
-
-    printf("Custo = %i\n", custo);
-    if (custo < menor_custo) menor_custo = custo;
-    printf("Menor custo = %i\n", menor_custo);
-
-    return custo;
 }
 
 // Função para trocar dois elementos em um array
